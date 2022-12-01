56fa26dc78f8b460a9d3e37ced81e0a5850a09da
krlosg07 <66976532+krlosg07@users.noreply.github.com>
2022-12-01 11:14:13 +0100

    Create udpserver.py

---
+++ udpserver.py
@@ -0,0 +1,24 @@
+#!usr/bin/python
+
+#import socket
+
+#msgtoclient = str.encode("Salut Client UDP")
+
+#sock = socket.socket(TODO,TODO)
+
+#host = TODO
+#port = TODO
+
+#sock.bind((host,port))
+#print("En attente d'un message du client...")
+#while True:
+
+	#msgClient = sock.recvfrom(1024)
+	#message = msgClient[0]
+	#addr = msgClient[1]
+	#clientMsg = "Messages reçus: {}".format(message)
+	#clientIP = "Adresse IP du client: {}".format(addr)
+	#print (clientMsg)
+	#print(clientIP)
+
+	#sock.sendto(msgtoclient,addr)
