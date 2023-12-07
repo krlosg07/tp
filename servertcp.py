a1afaf15f364afcef15ff79ce8023235f883142e
krlosg07 <66976532+krlosg07@users.noreply.github.com>
2023-12-07 11:54:35 +0100

    Update servertcp.py

--- servertcp.py
+++ servertcp.py
@@ -1,20 +1,16 @@
-#!/usr/bin/env python3
+import socket
 
-#import socket
+HOST = TODO
+PORT = TODO
 
-#HOST = TODO
-#PORT = TODO
-
-#with socket.socket(TODO, TODO) as s:
-    #s.bind((HOST, PORT))
-    #s.listen()
-    #conn, addr = s.accept()
-    #with conn:
-        #print('Connected by', addr)
-        #while True:
-            #data = conn.recv(1024)
-            #if not data:
-                #break
-            #print("Message reçu: %s",data)
-            #conn.sendall("J'ai bien reçu votre msg: "+data)
-        #s.close()
+with socket.socket(TODO, TODO) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = sock.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
