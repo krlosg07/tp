ff253bb7d098edea5c38d3f76fac11de2d2dee44
krlosg07 <66976532+krlosg07@users.noreply.github.com>
2023-12-07 12:01:35 +0100

    Update clienttcp.py

--- clienttcp.py
+++ clienttcp.py
@@ -4,8 +4,8 @@ HOST = TODO
 PORT = TODO
 
 with socket.socket(TODO, TODO) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
+    socket.connect((HOST, PORT))
+    socket.sendall(b"Hello, world")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
